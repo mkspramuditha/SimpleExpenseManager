2ae333c06b6f23061af95fc2a628fb557c3aeeaa
mkspramuditha <mkspramuditha@gmail.com>
2016-11-19 18:42:37 +0530

    DBConnect added

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistanceExpenseManager.java
@@ -0,0 +1,26 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import android.content.Context;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+
+/**
+ * Created by shan on 11/19/2016.
+ */
+
+public class PersistanceExpenseManager extends ExpenseManager {
+    private Context context= null;
+    public PersistanceExpenseManager(Context context) {
+        this.context =context;
+        setup();
+    }
+
+    public void setup(){
+        AccountDAO persestanceAccountDAO = new PersistanceAccountDAO(context);
+        setAccountsDAO(persestanceAccountDAO);
+
+        TransactionDAO persistanceTransactionDAO = new PersistanceTransactionDAO(context);
+        setTransactionsDAO(persistanceTransactionDAO);
+    }
+}
---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/database/DBConnect.java
@@ -0,0 +1,60 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by shan on 11/19/2016.
+ */
+
+public class DBConnect extends SQLiteOpenHelper {
+    protected static final String dbName = "140474R";
+    private static DBConnect dbConnect = null;
+    private static final int dbVersion = 1;
+
+    public static final String accountTableName = "Accounts";
+    public static final String bankAccountNo = "accountNo";
+    public static final String bankName = "bank";
+    public static final String accountHolder = "accountHolder";
+    public static final String balance = "balance";
+
+    public static final String transactionTableName = "Transactions";
+    public static final String transactionId = "transactionId";
+    public static final String date = "date";
+    public static final String accountNo = "accountNo";
+    public static final String expenceType = "expenseType";
+    public static final String amount = "amount";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String accountTableSql = String.format("CREATE TABLE %s(%s VARCHAR(20) NOT NULL PRIMARY KEY, %s VARCHAR(100) NULL, %s VARCHAR(100) NULL, %s DECIMAL(10,2) NULL)"
+                ,accountTableName,bankAccountNo,bankName,accountHolder,balance);
+
+        String transactionTableSql = String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,%s VARCHAR(100) NOT NULL, %s DATE NULL, %s DECIMAL(10,2) NULL,%s VARCHAR(100) NULL, FOREIGN KEY(%s) REFERENCES %s(%s))"
+                ,transactionTableName,transactionId,accountNo, date,amount,expenceType,accountNo,accountTableName, bankAccountNo);
+
+        db.execSQL(accountTableSql);
+        db.execSQL(transactionTableSql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS"+accountTableName);
+        db.execSQL("DROP TABLE IF EXISTS"+transactionTableName);
+        onCreate(db);
+    }
+
+    public DBConnect(Context context) {
+        super(context, dbName, null, dbVersion);
+    }
+
+    public static DBConnect getInstance(Context context){
+        if(dbConnect== null){
+            dbConnect = new DBConnect(context);
+        }
+        return dbConnect;
+    }
+}
